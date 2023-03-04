e8205315030666b71cfaa9366980d6b3b29fd9ca
Spurthi Eshwarappa <seshwarappa@saudia.com>
2023-03-04 11:23:10 +0530

    Initial Commit

---
+++ Airlines/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Airlines
+//
+//  Created by spurthi.eshwarappa on 04/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
